4f7a3f85a331d425756a4e17e55946b38298af3f
wonjunlim1 <87891575+wonjunlim1@users.noreply.github.com>
2023-06-24 11:07:19 +0900

    feat: main context

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ def get_group_feed_data(group_id, access_token):
         print("Error retrieving data from Facebook API")
         print(data)
         return []
+# Set your group ID and access token
+group_id = "{your group id}}"
+access_token = "{your access token}"
+
+# Call the function to get the group's feed data
+group_feed_data = get_group_feed_data(group_id, access_token)
+
+# json 객체로 변환 + utf-8로 인코딩
+json_data = json.dumps(group_feed_data, indent=4, ensure_ascii=False).encode("utf-8")
+
+# json 파일로 저장
+with open("results.json", "wb") as f:
+    f.write(json_data)
